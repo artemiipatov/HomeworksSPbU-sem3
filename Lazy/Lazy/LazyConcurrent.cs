c5fb56e3089dfc0734566395c4aa987ade7e9ec3
ArtiomPatov <patov.988@gmail.com>
2022-09-25 17:37:42 +0300

    Initial commit

---
+++ Lazy/Lazy/LazyConcurrent.cs
@@ -0,0 +1,36 @@
+﻿namespace Lazy;
+
+/// <summary>
+/// Lazy that can be used concurrently by multiple threads. It guarantees that there won't be any deadlocks or races.
+/// </summary>
+/// <typeparam name="T">Return type of a function.</typeparam>
+public class LazyConcurrent<T> : ILazy<T>
+{
+    public LazyConcurrent(Func<T?> func)
+    {
+        _func = func;
+    }
+
+    private bool _isCalculated = false;
+    
+    private object _locker = new();
+    
+    private Func<T?> _func;
+
+    private T? _result;
+    
+    public T? Get()
+    {
+        lock (_locker)
+        {
+            if (_isCalculated)
+            {
+                return _result;
+            }
+
+            _result = _func();
+            _isCalculated = true;
+            return _result;
+        }
+    }
+}
---
+++ Lazy/Lazy/LazySerial.cs
@@ -0,0 +1,31 @@
+﻿namespace Lazy;
+
+/// <summary>
+/// Lazy that does not support multithreading.
+/// </summary>
+/// <typeparam name="T">Return type of a function.</typeparam>
+public class LazySerial<T> : ILazy<T>
+{
+    public LazySerial(Func<T?> func)
+    {
+        _func = func;
+    }
+
+    private bool _isCalculated = false;
+    
+    private readonly Func<T?> _func;
+
+    private T? _result;
+
+    public T? Get()
+    {
+        if (_isCalculated)
+        {
+            return _result;
+        }
+
+        _result = _func();
+        _isCalculated = true;
+        return _result;
+    }
+}
