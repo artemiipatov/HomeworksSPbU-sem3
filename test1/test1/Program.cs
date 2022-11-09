798017cd8f11638e04916617179548eea680a089
ArtiomPatov <patov.988@gmail.com>
2022-11-09 13:09:58 +0300

    Add comparison

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -1,9 +1,38 @@
-﻿using test1;
+﻿using System.Diagnostics;
+using test1;
 
 if (args.Length != 1)
 {
     return;
 }
 
-var hash = CheckSumCalculator.CalculateCheckSumConcurrently(args[0]);
-Console.WriteLine(BitConverter.ToString(hash));
+const int n = 10;
+
+var calculationTime = new long[n];
+for (var i = 0; i < n; i++)
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    CheckSumCalculator.CalculateCheckSumSerially(args[0]);
+    stopwatch.Stop();
+    calculationTime[i] = stopwatch.ElapsedMilliseconds;
+}
+
+var expectedValue1 = calculationTime.Average();
+var deviation1 = Comparison.CalculateDeviation(calculationTime);
+Console.WriteLine(expectedValue1);
+Console.WriteLine(deviation1);
+
+for (var i = 0; i < n; i++)
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    CheckSumCalculator.CalculateCheckSumConcurrently(args[0]);
+    stopwatch.Stop();
+    calculationTime[i] = stopwatch.ElapsedMilliseconds;
+}
+
+var expectedValue2 = calculationTime.Average();
+var deviation2 = Comparison.CalculateDeviation(calculationTime);
+Console.WriteLine(expectedValue2);
+Console.WriteLine(deviation2);
