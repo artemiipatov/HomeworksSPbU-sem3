b51fd93c7697afce444fabef3a242f374f0fa763
ArtiomPatov <patov.988@gmail.com>
2022-11-20 19:53:38 +0300

    refactor

--- SimpleFTP/Client/Program.cs
+++ SimpleFTP/Client/Program.cs
@@ -1,8 +1,5 @@
 ﻿using var client = new Client.Client(8888, "localhost");
 
-await using var fileStream = new FileStream("data1.pdf", FileMode.Create);
-await client.GetAsync("Shurygin._.Analiticheskaya.geometriya.II.pdf", fileStream);
-
 var elementsInDirectory = await client.ListAsync("../../../");
 
 Console.WriteLine(elementsInDirectory.Item1);
