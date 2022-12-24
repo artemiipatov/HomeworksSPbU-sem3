064131b5c359e7180fa6bd3d9e6ad5afd0913e7e
ArtiomPatov <patov.988@gmail.com>
2022-12-25 00:41:14 +0300

    Add tests

--- MyNUnit/MyNUnit.Tests/MyNUnitTests.cs
+++ MyNUnit/MyNUnit.Tests/MyNUnitTests.cs
@@ -1,15 +1,48 @@
+using MyNUnit.Internal;
+using MyNUnit.Tests.TestFiles;
+
 namespace MyNUnit.Tests;
 
 public class MyNUnitTests
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void TestAbstractClass()
     {
+        var testType = new TestType(typeof(AbstractClassExample));
+        testType.Run();
     }
 
-    [Test]
-    public void Test1()
+    [TestCase(typeof(BeforeExample))]
+    [TestCase(typeof(BeforeClassExample))]
+    [TestCase(typeof(AfterExample))]
+    [TestCase(typeof(AfterClassExample))]
+    public void TestAssistantMethods(Type classExample)
     {
-        Assert.Pass();
+        var testType = new TestType(classExample);
+        var actualStatus = testType.TestUnitList.First().GeneralStatus; 
+        Assert.That(actualStatus, Is.EqualTo(Status.Succeed));
     }
+    
+    [Test]
+    public void TestMethods()
+    {
+        var testType = new TestType(typeof(TestCasesExample));
+
+        foreach (var testUnit in testType.TestUnitList)
+        {
+            Assert.That(testUnit.TestStatus, Is.EqualTo(GetExpectedStatus(testUnit)));
+        }
+    }
+
+    private Status GetExpectedStatus(TestUnit testUnit) =>
+        testUnit switch
+        {
+            { Method.Name: "Success" } => Status.Succeed,
+            { Method.Name: "Ignore" } => Status.Ignored,
+            { Method.Name: "ExpectedException" } => Status.CaughtExpectedException,
+            { Method.Name: "UnexpectedException" } => Status.Failed,
+            { Method.Name: "NonVoidReturnType" } => Status.NonVoidMethod,
+            { Method.Name: "HasArguments" } => Status.MethodHasArguments,
+            { Method.Name: "PrivateMethod" } => Status.NonPublicMethod,
+        };
 }
